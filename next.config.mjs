41ecb90a53aff93bab6945024b9802fc49a8f249
asharib <asharibarain4@gmail.com>
2024-09-02 23:52:08 +0500

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: 'i.imgflip.com',
+                protocol: 'https',
+            },
+            {
+                hostname: 'images.unsplash.com',
+                protocol: 'https',
+            },
+            {
+                hostname: 'm.media-amazon.com',
+                protocol: 'https',
+            },
+            {
+                hostname: 'edamam-product-images.s3.amazonaws.com',
+                protocol: 'https',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
